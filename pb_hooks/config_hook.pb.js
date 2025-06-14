315e9b5a08554517e01a760b158b096f5085703a
eddyizm <eddyizm@gmail.com>
2025-06-14 12:27:40 -0700

    feat: add hooks to get env vars from container

---
+++ pb_hooks/config_hook.pb.js
@@ -0,0 +1,13 @@
+routerAdd("GET", "/api/_/config", (e) => {
+    // adding some container variables for front end access
+    return e.json(200, {
+        siteTitle: $os.getenv("SITE_TITLE"),
+        appUrl: $os.getenv("APP_URL"),
+    })
+})
+
+// testing a new path/hook
+routerAdd("GET", "/hello/{name}", (e) => {
+    let name = e.request.pathValue("name")
+    return e.json(200, { "message": "Hello " + name })
+})
